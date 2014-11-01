867efa0b37445ca0ad64df008de313c340d431a8
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2014-11-01 17:54:56 -0500

    Initial Commit: Stock compojure template

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject address-book "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.2.0"]
+                 [ring/ring-defaults "0.1.2"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler address-book.core.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring-mock "0.1.5"]]}})
