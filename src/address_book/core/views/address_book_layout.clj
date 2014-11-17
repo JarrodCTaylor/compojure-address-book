f71fbc8d2faa1db63d4a27b9be955e6fb4fe4756
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2014-11-16 18:07:07 -0600

    Part 3: Style and templates

--- src/address_book/core/views/address_book_layout.clj
+++ src/address_book/core/views/address_book_layout.clj
@@ -1,11 +1,52 @@
 (ns address-book.core.views.address-book-layout
-  (:require [hiccup.page :refer [html5 include-css]]))
+  (:require [hiccup.page :refer [html5 include-css]]
+            [hiccup.core :refer [html h]]))
 
 (defn common-layout [& body]
   (html5
     [:head
-      [:title "Address Book"]
-      (include-css "/css/address-book.css")]
+     [:title "Address Book"]
+     (include-css "/css/address-book.css")
+     (include-css "http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css")]
     [:body
-      [:h1 "Address Book"]
-      body]))
+     [:div#wrapper
+      [:h1#content-title "Address Book"]
+      [:div#contacts
+       [:div.contact.header
+        [:div.column-1
+         [:i.icon-user.icon-style] " Name"]
+        [:div.column-2
+         [:i.icon-phone.icon-style] " Phone"]
+        [:div.column-3
+         [:i.icon-envelope.icon-style] " Email"]
+        [:div.clear-row]]
+       body]]]))
+
+(defn add-contact-form []
+  (html
+    [:div.contact
+      [:form {:action "/post" :method "post"}
+        [:div.column-1
+          [:input#name-input {:type "text" :name "name" :placeholder "Name"}]]
+        [:div.column-2
+          [:input#phone-input {:type "text" :name "phone" :placeholder "Phone"}]]
+        [:div.column-3
+          [:input#email-input {:type "text" :name "email" :placeholder "Email"}]]
+        [:button.button.add {:type "submit"} "Add "]]
+        [:div.clear-row]]))
+
+(defn read-contact [contact]
+  (html
+    [:div.contact
+      [:div.contact-text
+        [:div.column-1 (h (:name contact))]
+        [:div.column-2 (h (:phone contact))]
+        [:div.column-3 (h (:email contact))]]
+      [:div.button-group
+        [:form {:action (str "/edit/" (h (:id contact))) :method "get"}
+          [:button.button.edit {:type "submit"}
+            [:i.icon-pencil]]]
+        [:form {:action (str "/delete/" (h (:id contact))) :method "post"}
+          [:button.button.remove {:type "submit"}
+            [:i.icon-remove]]]]
+      [:div.clear-row]]))
--- test/address_book/core/address_book_tests.clj
+++ test/address_book/core/address_book_tests.clj
@@ -2,16 +2,20 @@
   (:use midje.sweet)
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [address-book.core.handler :refer :all]))
+            [address-book.core.handler :refer :all]
+            [address-book.core.routes.address-book-routes :refer [contacts]]))
 
 (facts "Example GET and POST tests"
 
   (fact "Test GET"
     (let [response (app (mock/request :get "/"))]
       (:status response) => 200
-      (:body response) => (contains "Example GET")))
+      (:body response) => (contains "<div class=\"column-1\">Jarrod Taylor</div>")
+      (:body response) => (contains "<div class=\"column-1\">Johnny Utah</div>")
+      (:body response) => (contains "<div class=\"column-1\">James Dalton</div>")))
 
   (fact "Test POST"
-    (let [response (app (mock/request :post "/post" {:example-post "Some data"}))]
-      (:status response) => 200
-      (:body response) => "You posted: Some data")))
+    (let [response (app (mock/request :post "/post" {:name "Bodhi" :phone "555-7890" :email "bells@beach.com"}))
+          new-contact (filter #(= 4 (:id %)) @contacts)]
+      (:status response) => 302
+      (:name (first new-contact)) => "Bodhi")))
