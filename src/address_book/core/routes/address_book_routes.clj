501ab66be8e34bf3f223822bdc32b3b60b7bd543
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-01-15 20:14:42 -0600

    Part 3: Style and templates

--- src/address_book/core/routes/address_book_routes.clj
+++ src/address_book/core/routes/address_book_routes.clj
@@ -1,15 +1,34 @@
 (ns address-book.core.routes.address-book-routes
-  (:require [compojure.core :refer :all]
-            [address-book.core.views.address-book-layout :refer [common-layout]]))
+  (:require [ring.util.response :as response]
+            [compojure.core :refer :all]
+            [address-book.core.views.address-book-layout :refer [common-layout
+                                                                 read-contact
+                                                                 add-contact-form]]))
 
-(defn example-post [request]
-  (let [post-value (get-in request [:params :example-post])]
-    (str "You posted: " post-value)))
+(def contacts (atom [{:id 1 :name "Jarrod Taylor" :phone "(555) 666-7777" :email "Jarrod@JarrodCTaylor.com"}
+                     {:id 2 :name "James Dalton"  :phone "(123) 567-8901" :email "Cooler@Roadhouse.com"}
+                     {:id 3 :name "Johnny Utah"   :phone "(543) 333-1234" :email "J.Utah@Buckeyes.com"}]))
 
-(defn example-get [request]
+(defn next-id []
+  (->>
+    @contacts
+    (map :id)
+    (apply max)
+    (+ 1)))
+
+(defn post-route [request]
+  (let [name  (get-in request [:params :name])
+        phone (get-in request [:params :phone])
+        email (get-in request [:params :email])]
+    (swap! contacts conj {:id (next-id) :name name :phone phone :email email})
+    (response/redirect "/")))
+
+(defn get-route [request]
   (common-layout
-    [:p "Example GET"]))
+    (for [contact @contacts]
+      (read-contact contact))
+    (add-contact-form)))
 
 (defroutes address-book-routes
-  (GET "/" [] example-get)
-  (POST "/post" [] example-post))
+  (GET  "/"     [] get-route)
+  (POST "/post" [] post-route))
