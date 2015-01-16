ec071cd01938ef2c3fafe1c12f474c529ae4d4e9
JarrodCTaylor <jarrod.c.taylor@gmail.com>
2015-01-15 20:12:23 -0600

    Part 1: Basic GET and POST

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [midje "1.6.3"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-midje "3.1.3"]]
   :ring {:handler address-book.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src/address_book/core/handler.clj
+++ src/address_book/core/handler.clj
@@ -3,9 +3,13 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn example-post [request]
+  (let [post-value (get-in request [:params :example-post])]
+    (str "You posted: " post-value)))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Example GET")
+  (POST "/post" [] example-post)
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))
